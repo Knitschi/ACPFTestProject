beba8454ad14c25654aa749b273e5983aeadf479
Knitschi <knitschi@freenet.de>
2018-02-21 19:53:38 +0100

    update CPFCMake

--- testpush.bat
+++ testpush.bat
@@ -1,5 +1,9 @@
 
 echo namespace{} >> Sources\CPackage\c.cpp
-git commit . -m"dummy change"
-git notes append -m "dontTr1gger"
+git commit . -m"dummy edit"
+echo namespace{} >> Sources\CPackage\c.cpp
+git commit . -m"dummy edit"
+echo namespace{} >> Sources\CPackage\c.cpp
+git commit . -m"dummy edit"
+::git notes append -m "dontTr1gger"
 git push origin refs/notes/* refs/heads/master refs/tags/*
