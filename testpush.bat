4ae42d7660af22004bb502f83b0bb5306841a5d9
Knitschi <knitschi@freenet.de>
2018-02-21 17:33:55 +0100

    dummy change

--- testpush.bat
+++ testpush.bat
@@ -1,5 +1,5 @@
 
 echo ; >> Sources\CPackage\c.cpp
-git commit . -m"whitespace"
-git notes append -m "dontTr1gger"
+git commit . -m"dummy change"
+:: git notes append -m "dontTr1gger"
 git push origin refs/notes/* refs/heads/master refs/tags/*
