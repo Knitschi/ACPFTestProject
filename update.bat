53584b09f3d5ad1103577fa73c9831c009b7dfbb
Knitschi <knitschi@freenet.de>
2018-02-22 19:08:54 +0100

    remove tag update

--- update.bat
+++ update.bat
@@ -2,10 +2,10 @@ git pull
 git submodule update --remote --merge
 
 :: Tags
-pushd Sources\APackage
-    git tag -l | xargs git tag -d
-    git fetch --tags
-popd
+::pushd Sources\APackage
+::    git tag -l | xargs git tag -d
+::    git fetch --tags
+::popd
 
-git tag -l | xargs git tag -d
-git fetch --tags
+::git tag -l | xargs git tag -d
+::git fetch --tags
