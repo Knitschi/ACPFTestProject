818a6b2a26807d7a32a4e6566b93ef6574cb389a
knitschi <knitschi@freenet.de>
2018-02-23 16:12:41 +0100

    fix package order

--- Sources/cpfOwnedPackages.cmake
+++ Sources/cpfOwnedPackages.cmake
@@ -1,1 +1,1 @@
-set( CPF_OWNED_PACKAGES "CPackage;APackage;DPackage;EPackage" CACHE STRING "The packages that are owned by this CI-project." FORCE)
+set( CPF_OWNED_PACKAGES "CPackage;DPackage;EPackage;APackage" CACHE STRING "The packages that are owned by this CI-project." FORCE)
