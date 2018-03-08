039ec450a5dadf2d27634e5d325105b92d174870
Knitschi <knitschi@freenet.de>
2018-03-08 11:24:17 +0100

    Add a test for CPackage and libSwitchWarningsOff as external package.

---
+++ Sources/cmake/Hunter/config.cmake
@@ -0,0 +1,7 @@
+# This file defines the versions of the libraries and tools that are obtained using the hunter package manager
+
+include(hunter_config)
+include(hunter_user_error)
+
+hunter_config(GTest VERSION 1.8.0-hunter-p11)
+
