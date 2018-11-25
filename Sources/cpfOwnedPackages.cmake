1d95626e51a17a16bf29e010758803d47161156a
Knitschi <knitschi@freenet.de>
2018-11-25 15:37:58 +0100

    - Make documentation its own package. - Adjust to changes in the cpfcmake api.

--- Sources/cpfOwnedPackages.cmake
+++
@@ -1,1 +0,0 @@
-set( CPF_OWNED_PACKAGES "CPackage;DPackage;EPackage;APackage" CACHE STRING "The packages that are owned by this CI-project." FORCE)
---
+++ Sources/cpfPackages.cmake
@@ -0,0 +1,15 @@
+set( CPF_PACKAGES 
+	EXTERNAL cotire
+	EXTERNAL cmake
+	EXTERNAL CPFCMake
+    EXTERNAL CPFBuildscripts
+    EXTERNAL libSwitchWarningsOff
+	EXTERNAL BPackage
+	EXTERNAL FPackage 
+	EXTERNAL GPackage
+    OWNED CPackage
+    OWNED DPackage
+    OWNED EPackage
+    OWNED APackage
+    OWNED documentation
+)
